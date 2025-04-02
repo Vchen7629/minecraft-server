dcded296587b4496bfaad705ebe6dce7a42e3426
Vchen7629 <chenvincent7629@gmail.com>
2025-04-02 00:33:20 -0700

    added 30 day ttl for objects in s3 storage to keep costs down

--- infrastructure/s3/main.tf
+++ infrastructure/s3/main.tf
@@ -5,3 +5,19 @@ resource "aws_s3_bucket" "name" {
         ManagedBy = "Terraform"
     }
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "name" {
+    bucket = var.bucket_name
+
+    rule {
+        id = "object-ttl-1-month"
+
+        expiration {
+            days = 30
+        }
+
+        filter { }
+
+        status = "Enabled"
+    }
+}
